3a63161428971ab5c4f1487138973e960da48df2
Zoruda <stronger6@gmail.com>
2021-01-28 14:51:20 -0700

    Level 1, Character, Bow and Arrow, Arrow

---
+++ Assets/ArrowPhysics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPhysics : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Vector2 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.x <= 0 && rb.velocity.y <= 0)
+        {
+            //Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + speed * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/BowAndArrow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowAndArrow : MonoBehaviour
+{
+    public GameObject Arrow;
+    public Rigidbody2D rb;
+    Vector2 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject arrow;
+            arrow = Instantiate(Arrow);
+
+            var movement = this.GetComponent<PlayerMovement>().movement;
+            var x = movement.x;
+            var y = movement.y;
+            speed = Vector2.zero;
+            if (x < 0)
+            {
+                speed.x = -1f + x;
+            }
+            
+            if (x > 0)
+            {
+                speed.x = 1f + x;
+            }
+            
+            if (y < 0)
+            {
+                speed.y = -1f + y;
+            }
+            
+            if (y > 0)
+            {
+                speed.y = 1f + y;
+            }
+            
+            if (x == 0 && y == 0)
+            {
+                speed.y = -1f;
+                speed.x = 0f;
+            }
+            Debug.Log(speed);
+            arrow.transform.position = this.transform.position;
+            arrow.GetComponent<ArrowPhysics>().speed = speed;
+            Destroy(arrow, 5f);
+        }    
+    }
+}
