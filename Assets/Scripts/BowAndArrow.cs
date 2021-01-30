0644425d4af955e9887ba21277c4ace83c290c24
Zoruda <stronger6@gmail.com>
2021-01-30 10:33:13 -0700

    Player Animation + Shooting

--- Assets/Scripts/BowAndArrow.cs
+++ Assets/Scripts/BowAndArrow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
@@ -9,24 +10,41 @@ public class BowAndArrow : MonoBehaviour
     public GameObject Arrow;
     public Rigidbody2D rb;
     Vector2 speed;
-    public float cooldown = 1f;
     private float timeStamp;
+    private GameObject Player;
+    private Player PlayerScript;
+    public Animator animator;
+    private bool fireFrame = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeStamp = Time.time + cooldown;
+        
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        GameObject arrow = null;
+        GameObject arrow;
         var rightx = Input.GetAxisRaw("Right X");
         var righty = Input.GetAxisRaw("Right Y");
-        if ((rightx != 0 || righty != 0) && (timeStamp <= Time.time))
+
+        if (rightx != 0 || righty != 0)
         {
-            var movement = this.GetComponent<PlayerMovement>().movement;
+            animator.SetFloat("Horizontal", rightx);
+            animator.SetFloat("Vertical", righty);
+            animator.SetFloat("Shoot", 1f);
+        }
+        else
+        {
+            animator.SetFloat("Shoot", 0f);
+        }
+        
+        if ((rightx != 0 || righty != 0) && fireFrame)
+        {
+            fireFrame = false;
+            var movement = this.GetComponent<Player>().movement;
             var x = movement.x;
             var y = movement.y;
             speed = Vector2.zero;
@@ -36,41 +54,45 @@ public class BowAndArrow : MonoBehaviour
             
             if (rightx < 0) //Left
             {
-                speed.x = -1f + x/2;
+                speed.x = -1f;
                 speed.y = y / 2;
                 rotation = 0;
                 offset.x = -this.GetComponent<Renderer>().bounds.size.x/2;
             }
             else if (rightx > 0) //Right
             {
-                speed.x = 1f + x/2;
+                speed.x = 1f;
                 speed.y = y / 2;
                 rotation = 180;
                 offset.x = this.GetComponent<Renderer>().bounds.size.x/2;
             }
             else if (righty < 0) //Down
             {
-                speed.y = -1f + y/2;
+                speed.y = -1f;
                 speed.x = x / 2;
                 rotation = 90;
                 offset.y = -this.GetComponent<Renderer>().bounds.size.y/2;
             }
             else if (righty > 0) //Up
             {
-                speed.y = 1f + y/2;
+                speed.y = 1f;
                 speed.x = x / 2;
                 rotation = -90;
-                offset.y = this.GetComponent<Renderer>().bounds.size.y/2;
+                offset.y = this.GetComponent<Renderer>().bounds.size.y/2 + 0.5f;
             }
 
             if (arrow)
             {
-                timeStamp = Time.time + cooldown;
                 arrow.transform.position = new Vector3(this.transform.position.x + offset.x, this.transform.position.y + offset.y, this.transform.position.z);
                 arrow.transform.eulerAngles = new Vector3(arrow.transform.eulerAngles.x, arrow.transform.eulerAngles.y, arrow.transform.eulerAngles.x + rotation);
                 arrow.GetComponent<ArrowPhysics>().speed = speed;
                 Destroy(arrow, 5f);
             }
         }
+    }
+
+    public void set()
+    {
+        fireFrame = true;
     }
 }
