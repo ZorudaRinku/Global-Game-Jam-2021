5a4814261b457ad3609ede41187db8876b2bc7d2
Zoruda <stronger6@gmail.com>
2021-01-31 11:03:09 -0700

    Starts + Stumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,21 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float attackSpeed = 5f;
     public float health = 3f;
-    public float stars = 0f;
+    public int stars = 0;
     public Rigidbody2D rb;
     public Vector2 movement;
     public Animator animator;
-    
+
+    public GameObject Star1;
+    public GameObject Star2;
+    public GameObject Star3;
+    private Image Star1Image;
+    private Image Star2Image;
+    private Image Star3Image;
+    public Sprite[] Images;
+
     // Start is called before the first frame update
     void Start()
-    {   
-        
+    {
+        Star1Image = Star1.GetComponent<Image>();
+        Star2Image = Star2.GetComponent<Image>();
+        Star3Image = Star3.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -29,10 +40,24 @@ public class Player : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        Star1Image.sprite = Images[Mathf.Clamp(stars, 0, 5)];
+        Star2Image.sprite = Images[Mathf.Clamp(stars + 1, 6, 13)];
+        Star3Image.sprite = Images[Mathf.Clamp(stars + 2, 14, 23)];
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "Stump")
+        {
+            Destroy(other.transform.GetChild(0).gameObject);
+            stars++;
+        }
     }
 }
