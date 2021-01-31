9cb5cc74707482e964d5a2449747d33d874521bd
RussAMR <44710713+RussAMR@users.noreply.github.com>
2021-01-31 01:40:21 -0700

    3 Hours of unsaved progress now saved

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("NewMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
